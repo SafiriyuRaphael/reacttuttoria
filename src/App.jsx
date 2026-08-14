bc1bf729d8656fc93eae3b3788bdafa4b3eec3e9
NEW_GITHUB_USERNAME <toniaroyce@gmail.com>
2026-08-14 14:25:50 +0100

    new beginnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Header from "./Header";
+
 import Hero from "./hero";
 import Heroine from "./Heroine";
 const App = () => {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,33 @@
 import Button from "./components/button";
 import { NAVLINKS } from "./constants/navItems";
 
+// xs  --  small device
+// sm  --   medium device
+// md  -- large device
+// lg   -- tabs / desktop
+// xl  -- large desktop
+// 2xl  --  largest size
+
+//padding
+// px
+// py
+// p
+// pl
+// pr
+
+////////////////////////////////////////////////////////////
+
 const Header = () => {
   return (
-    <header>
-      <h1>Raph tutorial</h1>
+    <header className="flex items-center  justify-between px-4 bg-[#fef3c7]">
+      <h1 className="text-4xl text-[#dc2626]/20 hover:text-red-700 duration-800 transition-all">
+        Raph tutorials
+      </h1>
       <nav>
         <ul>
           {/* <li>
             <a href="/">Home</a>
-          </li>
-    
+          </li>  
           <li>
             <a href="/contact">Contact</a>
           </li> */}
